350bfd283eccd6a71e3e1bb9d78ab78826d700cd
Kai Hong <ricedev10@gmail.com>
2025-12-15 21:57:32 -0500

    finds winner when dropping disk

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -1,21 +1,91 @@
-# frozen-string-literal: true
-
 # Command line interface game for playing connect against computer or other player
 class ConnectFour
+  attr_reader :winner
+
   def initialize(columns = 7, rows = 6)
     @grid = make_grid(columns, rows)
+    @winner = nil
+    @columns = columns
+    @rows = rows
   end
 
   def drop_disk(column, id)
     valid, msg = valid_column?(column)
     raise StandardError, msg unless valid
 
-    @grid[column].each_index do |i|
-      @grid[column][i] = id and break if @grid[column][i].nil?
+    @grid[column].each_index do |row|
+      next unless @grid[column][row].nil?
+
+      @grid[column][row] = id
+      check_winner(column, row)
+      break
     end
   end
 
+  def to_s
+    out = ''
+    @rows.times do |row|
+      @columns.times do |column|
+        # each row (X | X | X ...)
+        # start from top most row when adding to string
+
+        out << "#{@grid[column][@rows - row - 1] || '___'} | "
+      end
+      out << "\n"
+    end
+
+    out
+  end
+
   private
+
+  def each_row_at(index = 0)
+    @grid.each do |column|
+      yield(column[index])
+    end
+  end
+
+  def find_winner_at(iterator)
+    last = nil
+    count = 1
+
+    iterator.call(proc do |e|
+      last == e && !e.nil? ? count += 1 : count = 1
+      last = e
+
+      @winner = last and return true if count == 4
+    end)
+
+    false
+  end
+
+  def check_winner(last_column, last_row)
+    # check column
+    column = @grid[last_column]
+    return if find_winner_at(lambda { |b|
+      column.each(&b)
+    })
+
+    # check row
+    return if find_winner_at(lambda { |b|
+      each_row_at(last_row, &b)
+    })
+
+    # check diagonal
+    left_most = last_column
+    bottom_most = last_row
+
+    left_most -= 1 and bottom_most -= 1 while left_most.positive? && bottom_most.positive?
+
+    find_winner_at(proc { |b|
+      diagonal = @grid[left_most][bottom_most]
+      while left_most <= @columns && bottom_most <= @rows
+        b.call(diagonal)
+        left_most += 1
+        bottom_most += 1
+      end
+    })
+  end
 
   def valid_column?(column)
     unless column.between?(0, @grid.count - 1)
--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -15,6 +15,10 @@ describe ConnectFour do
   end
 
   describe '#drop_disk' do
+    before do
+      allow(game).to receive(:check_winner)
+    end
+
     context 'when one disk is added to leftmost column' do
       it 'has a disk in first column' do
         game.drop_disk(0, :player1)
@@ -34,6 +38,71 @@ describe ConnectFour do
         grid = game.instance_variable_get(:@grid)
         expect(grid[0][0]).to eq(:player1)
         expect(grid[0][1]).to eq(:player1)
+      end
+    end
+  end
+
+  describe '@winner' do
+    context 'when dropping 4 disks in a column' do
+      before do
+        4.times { game.drop_disk(0, :player1) }
+      end
+
+      it 'has a winner' do
+        expect(game.winner).to eq(:player1)
+      end
+    end
+
+    context 'when dropping 4 disks in a row' do
+      before do
+        4.times { |i| game.drop_disk(i, :player1) }
+      end
+
+      it 'has a winner' do
+        expect(game.winner).to eq(:player1)
+      end
+    end
+
+    context 'when dropping 4 disks on second row on top of random disks' do
+      before do
+        game.drop_disk(1, :player1)
+        game.drop_disk(2, :player2)
+        game.drop_disk(3, :player2)
+        game.drop_disk(4, :player1)
+        4.times { |i| game.drop_disk(i + 1, :player1) }
+      end
+
+      it 'has a winner' do
+        expect(game.winner).to eq(:player1)
+      end
+    end
+
+    context 'when dropping 4 disks on top of disk in column' do
+      before do
+        2.times { game.drop_disk(2, :player2) }
+        4.times { game.drop_disk(2, :player1) }
+      end
+
+      it 'has player1 winner' do
+        expect(game.winner).to eq(:player1)
+      end
+    end
+
+    context 'when dropping disks diagonally' do
+      before do
+        game.drop_disk(1, :player1)
+
+        game.drop_disk(2, :player2)
+        game.drop_disk(2, :player1)
+
+        2.times { game.drop_disk(3, :player2) }
+        game.drop_disk(3, :player1)
+
+        3.times { game.drop_disk(4, :player2) }
+        game.drop_disk(4, :player1)
+      end
+      it 'has a winner' do
+        expect(game.winner).to eq(:player1)
       end
     end
   end
